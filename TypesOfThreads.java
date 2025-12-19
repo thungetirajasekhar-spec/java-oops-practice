4126ca74f64be021ccbda1161a8b4129247db684
thungetirajasekhar-spec <thungetirajasekhar@gmail.com>
2025-12-19 19:39:30 +0530

    Add files via upload

---
+++ TypesOfThreads.java
@@ -0,0 +1,40 @@
+package LearningJava;
+class Demo1 extends Thread
+{
+	public void run()
+	{
+		System.out.println("");
+		
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println("");
+			
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(Exception e)
+			{
+				System.out.println("");
+			}
+		}
+		System.out.println("");
+	}
+}
+class Demo2 extends Thread
+{
+	public void run()
+	{
+		System.out.println("");
+		for (int i=)
+	}
+}
+
+public class TypesOfThreads {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
