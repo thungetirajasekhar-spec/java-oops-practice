92aa9059ae26c14e26388ec6aa546bc9edda59c9
thungetirajasekhar-spec <thungetirajasekhar@gmail.com>
2025-12-21 22:55:32 +0530

    Add files via upload

---
+++ ExampleForTreeSet.java
@@ -0,0 +1,15 @@
+import java.util.TreeSet;
+
+public class ExampleForTreeSet {
+
+	public static void main(String[] args) {
+		 TreeSet<Integer> ts=new TreeSet<Integer>();
+		 ts.add(10);
+		 ts.add(50);
+		 ts.add(40);
+		 ts.add(5);
+		 System.out.println(ts);
+
+	}
+
+}
---
+++ HashTable.java
@@ -0,0 +1,30 @@
+import java.util.Hashtable;
+public class HashTable {
+
+	public static void main(String[] args) {
+		 /*Hashtable ht=new Hashtable();
+		 ht.put("Name","Raj");
+		 ht.put("age",22);
+		 ht.put("email","raj@gmail.com");
+		 System.out.println(ht);
+		 */
+		/*String s=new String("raj");
+		
+		s.concat("sekhar");
+		System.out.println(s);
+		*/
+		StringBuffer sb=new StringBuffer("raj");
+		System.out.println(sb);
+		sb.append("sekhar");
+		System.out.println(sb);
+		
+		StringBuilder sb1=new StringBuilder("raj");
+		System.out.println(sb1);
+		sb.append("sekhar");
+		System.out.println(sb1);
+		
+		
+		
+	}
+
+}
